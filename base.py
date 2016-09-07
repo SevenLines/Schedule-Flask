c9abcd8a43601e3936576a4432f1a2b5ee7e8177
mick <mmailm@mail.ru>
2016-09-07 19:59:24 +0800

    Add new styles, fix schedule output

--- base.py
+++ base.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+from jinja_filters import *
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://disp:d7i7s7p@DATASCHEDULE'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+app.jinja_env.filters['dayofweek'] = dayofweek
+app.jinja_env.filters['parainfo'] = parainfo
+
 db = SQLAlchemy(app)
--- views.py
+++ views.py
@@ -1,3 +1,4 @@
+import json
 from itertools import groupby
 
 from flask.templating import render_template
@@ -19,11 +20,34 @@ def group_schedule(kont_id):
     group = Kontgrp.query.get(kont_id)
     raspis = Raspis.get_for_kontgrp(group)
 
-    schedule = {day: {
-        para: list(lessons) for para, lessons in groupby(paras, lambda x: (x.para - 1) % 7 + 1)
-    } for day, paras in groupby(raspis, lambda x: x.day)}
+    # schedule = {para: {
+    #     day: list(lessons) for day, lessons in groupby(paras, lambda x: x.para)
+    # } for para, paras in groupby(raspis, lambda x: (x.day - 1) % 7 + 1)}
+
+    schedule = {
+        para: {
+            day: {
+                'everyweek': None,
+                'odd': None,
+                'even': None,
+            } for day in [1,2,3,4,5,6]
+        } for para in [0, 1,2,3,4,5,6,7,8]
+    }
+
+    for lesson in raspis:
+        if lesson.everyweek == 1:
+            if lesson.day > 7:
+                week = 'even'
+            else:
+                week = 'odd'
+        else:
+            week = 'everyweek'
+
+        schedule[lesson.para][(lesson.day - 1) % 7 + 1][week] = lesson
 
     return render_template("groups/schedule.html", **{
         "group": group,
+        "groups_json": json.dumps([{"id": group.id, "text": group.title}
+                              for group in Kontgrp.query.order_by(Kontgrp.title)]),
         "schedule": schedule
     })
